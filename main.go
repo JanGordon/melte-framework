7875cb8bbd37b683c8523cee00fdae2ded851b0e
Jan <timmyjan46@gmail.com>
2022-10-09 10:28:08 +0100

    cleanup main.go

--- main.go
+++ main.go
@@ -1,20 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/JanGordon/melte-framework/cli"
 )
 
 func main() {
-	// fmt.Println("HEllo World")
-	// compile.Transform()
-	c := 0
-	os.Create(fmt.Sprintf("da%d.js", c))
-	startCli()
-}
-
-func startCli() {
 	cli.Start()
 }
