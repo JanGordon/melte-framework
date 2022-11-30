f8bedfe8806e1ceb3b4b3a3fb5c8b7515fd944a8
Jan <timmyjan46@gmail.com>
2022-11-30 21:09:51 +0000

    ssr variables

--- test.js
+++ test.js
@@ -1,6 +1,18 @@
-let hello = "hello"
-
-Object.observe()
-
-
-console.log(hello)
+var obj = {"hello"}
+  
+  var proxied = new Proxy(obj, {
+    get: function(target, prop) {
+      console.log({ type: 'get', target, prop });
+      return Reflect.get(target, prop);
+    },
+    set: function(target, prop, value) {
+      console.log({ type: 'set', target, prop, value });
+      return Reflect.set(target, prop, value);
+    }
+  });
+  
+  proxied.bar = 2;
+  // {type: 'set', target: <obj>, prop: 'bar', value: 2}
+  
+  proxied.foo;
+  // {type: 'get', target: <obj>, prop: 'bar'}
